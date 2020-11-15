a8a0925cc6e4f21a92fc9c6dd940917b35a11305
Luke Becker <luke.w.becker32@gmail.com>
2020-11-15 09:30:10 -0600

    updates

--- acquire.py
+++ acquire.py
@@ -3,3 +3,50 @@ from requests import get
 from bs4 import BeautifulSoup
 import os
 
+# 1st Acquire function:
+
+def get_article_text(url):
+    # if we already have the data, read it locally
+    if os.path.exists('article.txt'):
+        with open('article.txt') as f:
+            return f.read()
+
+    # otherwise go fetch the data
+    url = url
+    headers = {'User-Agent': 'Codeup Data Science'}
+    response = get(url, headers=headers)
+    soup = BeautifulSoup(response.text)
+    article = soup.find('div', class_='jupiterx-post-content')
+
+    # save it for next time
+    with open('article.txt', 'w') as f:
+        f.write(article.text)
+
+    return article.text
+
+
+
+# First raw attempt at inshorts news scraping:
+
+urls = ['https://codeup.com/codeups-data-science-career-accelerator-is-here/', 'https://codeup.com/data-science-myths/', 'https://codeup.com/data-science-vs-data-analytics-whats-the-difference/', 'https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/','https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/']
+def get_blog_articles(url_list):
+    final = [] 
+    for x in url_list:
+        url = x
+        headers = {'User-Agent': 'Codeup Data Science'} # Some websites don't accept the pyhon-requests default user-agent
+        response = get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        article_title = soup.title.string
+        article = soup.find('div', class_='jupiterx-post-content')
+        article_text = article.text
+        item = {
+            'title': article_title,
+            'content': article_text
+        }
+        final.append(item)
+            # save it for next time
+    with open('article.txt', 'w') as f:
+        f.write(article.text)
+    return final
+
+# Big thanks to Matt for his help getting me pointed in the right direction on this!
